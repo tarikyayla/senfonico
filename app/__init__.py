c786f130c5d0af15d4f141499b13d61de5d98550
Tarık <tarikyayla33@gmail.com>
2018-09-20 01:52:22 +0300

    updated database and scripts

---
+++ app/__init__.py
@@ -0,0 +1,68 @@
+
+from flask import Flask,jsonify,request,render_template
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+from flask_marshmallow import Marshmallow
+
+app = Flask(__name__)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+mm = Marshmallow(app)
+#Table
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(50))
+    body = db.Column(db.String(140))
+    date = db.Column(db.Date, index=True, default=datetime.utcnow().date())
+
+class Schema(mm.Schema):
+    class Meta:
+        fields = ('id','title', 'body','date')
+        ordered = True
+
+pSchema = Schema()
+postsSchema = Schema(many=True)
+ 
+# API ROUTING
+@app.route("/api/add", methods=["POST"])
+def new_post():
+    title = request.form['title']
+    body = request.form['body']
+    new_post = Post(title=title,body=body)
+    db.session.add(new_post)
+    db.session.commit()
+    result = pSchema.dump(new_post)
+    return render_template("base.html")
+"""
+Aslında ilk başta yazdığımda json verisi gönderirim diye düşünüyordum, sonrasında tekrar uğraşmamak için form şeklinde değiştirdim.
+
+@app.route("/api/add", methods=["POST"])
+def new_post():
+    title = request.json['title']
+    body = request.json['body']
+    new_post = Post(title=title,body=body)
+    db.session.add(new_post)
+    db.session.commit()
+    result = pSchema.dump(new_post)
+    return render_template("base.html")
+
+"""
+
+@app.route("/api/posts", methods=["GET"])
+def get_posts():
+    posts = Post.query.all()
+    result = postsSchema.dump(posts)
+    return jsonify(result.data)
+
+@app.route("/api/post/<id>", methods=["GET"])
+def get_post(id):
+    post = Post.query.get(id)
+    result = pSchema.dump(post)
+    return jsonify(result.data)
+
+
+@app.route("/")
+@app.route("/index")
+def index():
+    return render_template("base.html")
---
+++ app/static/ext/script.js
@@ -0,0 +1,36 @@
+$(document).ready(function () {
+   $.getJSON("http://localhost/api/posts",function (data) {
+           $.each(data,function(index,element){
+               var title = element.title;
+               if(title.length > 50){
+                   title = title.substring(0,25) +"..."
+               }
+               $(".postlist").append("<li><a href='#' value='" + element.id +"'>"+ title +"</a><span>"+element.date +"</span></li>");
+           })
+       }
+   );
+  
+    $(".postlist").on("click","li a",function(e){
+        e.preventDefault();
+        var id = $(this).attr("value");
+        $.getJSON("http://localhost/api/post/"+id,function (data) {
+                var titleElement = document.getElementById("modalTitle");
+                var bodyElement = document.getElementById("modalBody");
+                titleElement.innerText = data.title;
+                bodyElement.innerText = data.body;
+            }
+            );
+            $(this).attr("data-toggle", "modal");
+            $(this).attr("data-target", "#postmodal");
+        });
+
+    $(".navbar li a").click(function (e){
+        e.preventDefault();
+        $(this).attr("data-toggle", "modal");
+        $(this).attr("data-target", ".bd-example-modal-sm");
+        setTimeout(function() {
+            $(".bd-example-modal-sm").modal('hide');
+          }, 5000);
+    });
+});
+
